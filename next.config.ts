c990e53c96407c2e11d1022150ac30840682c62a
alexplex <alexplex@users.noreply.github.com>
2025-11-15 09:32:38 +0100

    Add Prisma engine files to output tracing

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   experimental: {
     serverComponentsExternalPackages: ['@prisma/client', 'prisma'],
   },
+  outputFileTracingIncludes: {
+    '/api/**/*': ['./node_modules/.prisma/client/libquery_engine-*'],
+  },
 };
 
 export default nextConfig;
